072e61a3e360ed3e4519448d06cf52f9bd8830a8
marcia-ferreira57 <marciacomarte@gmail.com>
2026-05-11 22:57:02 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -7,17 +7,19 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = Guerreiro;
+        this.nivel = 1;
+        this.pontosDeVida = 100;
+        this.poderBase = 10.0;
 
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("--- Status do Personagem ---" );
+        System.out.println("--- Classe: ---" this.classe);
+        System.out.println("--- Nivel: ---" this.nivel);
+        System.out.println("--- pontosDeVida: ---") this.pontosDeVida;
+        System.out.println("--- podeBase ---" this.poderBase);
 
     }
 }
